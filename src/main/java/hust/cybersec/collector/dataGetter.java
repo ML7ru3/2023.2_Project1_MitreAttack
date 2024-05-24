f7723ebd990d189529d08390b16e41848074ca23
ML7ru3 <quangtrung3112004@gmail.com>
2024-05-24 15:28:59 +0700

    fix dataGetter (now can download file)

--- src/main/java/hust/cybersec/collector/dataGetter.java
+++ src/main/java/hust/cybersec/collector/dataGetter.java
@@ -1,25 +1,37 @@
 package hust.cybersec.collector;
+
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
 import java.io.IOException;
+import java.io.*;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import org.json.JSONObject;
 
 public class dataGetter {
-    public static void main(String args[]) {
-
+    static void retrieveData(String url, String filename) throws IOException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://api.github.com/repos/redcanaryco/atomic-red-team/contents/atomics/Indexes/index.yaml"))
+                .uri(URI.create(url))
                 .GET()
                 .build();
         HttpClient client = HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.ALWAYS)
                 .build();
         try {
-            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() == 200) {
-                System.out.println(response.body());
+                String responseBody = response.body();
+                JSONObject jsonObject = new JSONObject(responseBody);
+                String downloadUrl = jsonObject.getString("download_url");
+                try {
+                    downloadData(downloadUrl, filename);
+                } catch (IOException e) {
+                    System.err.println("oh no");
+                }
             } else {
                 System.err.println("Failed to fetch data. Status code: " + response.statusCode());
             }
@@ -27,6 +39,28 @@ public class dataGetter {
             System.err.println("Failed to fetch data.");
             e.printStackTrace();
         }
-
     }
+
+    static void downloadData(String downloadUrl, String filename) throws IOException {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(downloadUrl).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(filename)) {
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to fetch data.");
+        }
+    }
+
+//
+//    public static void main(String[] args) {
+////        FORM:
+////        try {
+////            retrieveData("https://api.github.com/repos/redcanaryco/atomic-red-team/contents/atomics/Indexes/index.yaml", "index.yaml");
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////        }
+//    }
 }

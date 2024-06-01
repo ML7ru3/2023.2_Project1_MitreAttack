e1b58e13e5edc95e5d4d37e9b21dca085c070606
ML7ru3 <quangtrung3112004@gmail.com>
2024-06-01 09:20:23 +0700

    update data

--- src/main/java/hust/cybersec/model/AtomicRedTeam.java
+++ src/main/java/hust/cybersec/model/AtomicRedTeam.java
@@ -150,9 +150,9 @@ public class AtomicRedTeam extends MitreAttackFramework {
         final String[] MITRE_URL = {"https://api.github.com/repos/mitre-attack/attack-stix-data/contents/enterprise-attack/enterprise-attack.json",
                 "https://api.github.com/repos/mitre-attack/attack-stix-data/contents/ics-attack/ics-attack.json",
                 "https://api.github.com/repos/mitre-attack/attack-stix-data/contents/mobile-attack/mobile-attack.json"};
-        final String[] NAME_FILE = {"/mitre/enterprise-attack.json",
-                "/mitre/ics-attack.json",
-                "/mitre/mobile-attack.json"};
+        final String[] NAME_FILE = {"enterprise-attack.json",
+                "ics-attack.json",
+                "mobile-attack.json"};
 
         for (int i = 0; i < 3; i++){
             dataGetter mitreRetriever = new dataGetter(MITRE_URL[i], NAME_FILE[i]);

5fcad4129568c2b4480934724e4aa16c0ec73f55
ML7ru3 <quangtrung3112004@gmail.com>
2024-06-22 01:04:40 +0700

    fixing

--- src/main/java/hust/cybersec/UI/Controller/ChartScreen.java
+++ src/main/java/hust/cybersec/UI/Controller/ChartScreen.java
@@ -8,16 +8,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ChartScreen extends Application {
-
+    DataProcessing data;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        data = new DataProcessing();
         final String SCREEN_FXML_FILE_PATH = "src/main/java/hust/cybersec/UI/view/MITRE_ATT&CK_chart.fxml";
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource(SCREEN_FXML_FILE_PATH));
-        ChartController chartController = new ChartController(new DataProcessing());
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(SCREEN_FXML_FILE_PATH));
+        ChartController chartController = new ChartController(data);
         fxmlLoader.setController(chartController);
         Parent root = fxmlLoader.load();
-
 
         Scene scene = new Scene(root);
         primaryStage.setTitle("ATOMIC ANALYZER");
